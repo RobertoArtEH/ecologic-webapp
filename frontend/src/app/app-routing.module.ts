b469c94546f4cfe5ef1b06ddb880b475110a48a7
Roberto Esqueda <roberto.raeh@gmail.com>
2020-04-05 19:17:44 -0500

    Add: Home y rutas hijas

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+// Components
+import { HomeComponent } from './components/home/home.component';
+import { DashboardComponent } from './components/home-children/dashboard/dashboard.component';
+import { MembersComponent } from './components/home-children/members/members.component';
+import { WeatherComponent } from './components/home-children/weather/weather.component';
+import { WaterComponent } from './components/home-children/water/water.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: '', 
+    component: HomeComponent,
+    children: [
+      { path: '', component: DashboardComponent },
+      { path: 'miembros', component: MembersComponent },
+      { path: 'clima', component: WeatherComponent },
+      { path: 'riego', component: WaterComponent }
+    ] 
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { MembersComponent } from './components/home-children/members/members.component';
+import { WeatherComponent } from './components/home-children/weather/weather.component';
+import { WaterComponent } from './components/home-children/water/water.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MembersComponent,
+    WeatherComponent,
+    WaterComponent
   ],
   imports: [
     BrowserModule,
