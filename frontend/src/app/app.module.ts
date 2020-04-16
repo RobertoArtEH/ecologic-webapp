41abab4df36269d90795977426bcdf5ace646bb1
Roberto Esqueda <roberto.raeh@gmail.com>
2020-04-16 01:16:19 -0500

    Add: Imports en app module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,11 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { MembersComponent } from './components/home-children/members/members.component';
 import { WeatherComponent } from './components/home-children/weather/weather.component';
 import { WaterComponent } from './components/home-children/water/water.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { RegisterComponent } from './components/auth/register/register.component';
 
 @NgModule({
   declarations: [
@@ -14,11 +18,15 @@ import { WaterComponent } from './components/home-children/water/water.component
     HomeComponent,
     MembersComponent,
     WeatherComponent,
-    WaterComponent
+    WaterComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
