6cdb19e4b39fc605cd5e4c2c6a5ab7953c456249
Roberto Esqueda <roberto.raeh@gmail.com>
2020-04-16 01:17:27 -0500

    Add: Rutas de login y register + guards en rutas

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -7,18 +7,25 @@ import { DashboardComponent } from './components/home-children/dashboard/dashboa
 import { MembersComponent } from './components/home-children/members/members.component';
 import { WeatherComponent } from './components/home-children/weather/weather.component';
 import { WaterComponent } from './components/home-children/water/water.component';
+import { RegisterComponent } from './components/auth/register/register.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   { 
     path: '', 
     component: HomeComponent,
+    canActivate: [AuthGuard],
+    canActivateChild: [AuthGuard],
     children: [
       { path: '', component: DashboardComponent },
       { path: 'miembros', component: MembersComponent },
       { path: 'clima', component: WeatherComponent },
       { path: 'riego', component: WaterComponent }
     ] 
-  }
+  },
+  { path:'login', component: LoginComponent, canActivate: [AuthGuard], data: { route: 'auth'} },
+  { path:'registro', component: RegisterComponent, canActivate: [AuthGuard], data: { route: 'auth'} }
 ];
 
 @NgModule({
