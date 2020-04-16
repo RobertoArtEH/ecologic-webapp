cebfffe4231c41cd30d69c869bf93137cb6e3227
Roberto Esqueda <roberto.raeh@gmail.com>
2020-04-16 01:19:56 -0500

    Add: Mostrar miembros, mostrar status de los miembros si el usuario es admin

--- frontend/src/app/components/home-children/members/members.component.ts
+++ frontend/src/app/components/home-children/members/members.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-members',
@@ -6,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./members.component.scss']
 })
 export class MembersComponent implements OnInit {
+  user: User;
+  members;
 
-  constructor() { }
+  constructor(private userService:UserService) { }
 
   ngOnInit(): void {
+    this.user = JSON.parse(localStorage.getItem('user'));
+
+    this.userService.getUsers().subscribe(response => this.members = response);
   }
 
 }
