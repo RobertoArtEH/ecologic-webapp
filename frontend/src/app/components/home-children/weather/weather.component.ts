38ddd6ec9348c5e6e45e63e62fe21b129544c1eb
eliud loza <eliudlozautt@hotmail.com>
2020-04-22 13:00:49 -0500

    Add images, descp, temp

--- frontend/src/app/components/home-children/weather/weather.component.ts
+++ frontend/src/app/components/home-children/weather/weather.component.ts
@@ -8,10 +8,43 @@ import { WeatherService } from 'src/app/services/weather.service';
 })
 export class WeatherComponent implements OnInit {
   day;
+  urlBrokenClouds = "";
   constructor(private weahterService: WeatherService) { }
 
   ngOnInit(): void {
-    this.weahterService.nextDays().subscribe(arg => this.day = arg);
+    this.weahterService.nextDays().subscribe(arg => {
+      this.day = arg;
+    });
+  }
+  getBadge(description) {
+    switch (description) {
+      case "broken clouds":
+        return "#2ECC71";
+      break;
+      case "clear sky":
+        return "#2874A6";
+      break;
+      case "light rain":
+        return "#F1C40F";
+      break;
+      default:
+        break;
+    }
+  }
+  getBackground(description) {
+    switch (description) {
+      case "broken clouds":
+        return 'https://live.staticflickr.com/1828/28636482297_bd428f26e8_b.jpg';
+      break;
+      case "clear sky":
+        return 'https://www.publicdomainpictures.net/pictures/90000/nahled/clear-blue-sky-landscape.jpg';
+      break;
+      case "light rain":
+        return 'https://p7.hiclipart.com/preview/810/1006/528/peshawar-light-rain-drop-precipitation-rain.jpg';
+      break;
+      default:
+        break;
+    }
   }
 
 }
